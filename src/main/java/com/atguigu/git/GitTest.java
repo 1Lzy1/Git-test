eeb6d14f306c9a9cf5c2f2162059e19bf1229f7f
Lzy <1144320426@qq.com>
2022-07-25 12:37:10 +0800

    hot2 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,8 +4,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello 2");
-        System.out.println("hello 3");
-        System.out.println("hello 4");
-        System.out.println("hello 5");
     }
 }
